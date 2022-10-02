5d38838b82eb75de15a2ad100ad7e74547f3ec2c
Bentebent <henriksson.tim@gmail.com>
2022-10-02 22:15:40 +0200

    Fixed splash tower

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -7,31 +7,35 @@ namespace LD51 {
         // Start is called before the first frame update
         Player player = null;
 
-        bool destroyMe = false;
         void Start() {
             player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
         }
 
         // Update is called once per frame
         void Update() {
-            if (destroyMe) {
-                Destroy(gameObject);
-            }
 
-            if (!destroyMe) {
-                destroyMe = true;
-            }
-        }
-
-        private void OnTriggerEnter(Collider other) {
-            if (other.gameObject.layer == Layers.Enemy) {
-                var unit = other.GetComponent<PathingUnit>();
+            var colliders = Physics.OverlapSphere(transform.position, 5.0f, Layers.GetMask(Layers.Enemy));
+            foreach (var collider in colliders) {
+                var unit = collider.GetComponent<PathingUnit>();
                 unit.health -= 25;
                 if (unit.health <= 0) {
                     player.AddMoney(unit.value);
                     Destroy(unit.gameObject);
                 }
             }
+
+            Destroy(gameObject);
+        }
+
+        private void OnTriggerEnter(Collider other) {
+            //if (other.gameObject.layer == Layers.Enemy) {
+            //    var unit = other.GetComponent<PathingUnit>();
+            //    unit.health -= 25;
+            //    if (unit.health <= 0) {
+            //        player.AddMoney(unit.value);
+            //        Destroy(unit.gameObject);
+            //    }
+            //}
         }
 
     }
