59d4537559a3a25cb07b8a1f18e64041c01a087e
Bentebent <henriksson.tim@gmail.com>
2022-10-02 19:28:30 +0200

    I did tower things

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -6,14 +6,21 @@ namespace LD51 {
     public class Explosion : MonoBehaviour {
         // Start is called before the first frame update
         Player player = null;
+
+        bool destroyMe = false;
         void Start() {
-            Destroy(gameObject, 1.0f);
             player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
         }
 
         // Update is called once per frame
         void Update() {
+            if (destroyMe) {
+                Destroy(gameObject);
+            }
 
+            if (!destroyMe) {
+                destroyMe = true;
+            }
         }
 
         private void OnTriggerEnter(Collider other) {
